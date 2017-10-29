53903768e0f3e513e4ed3675902041ced759d22e
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2017-10-29 23:38:45 +0900

    Add sample code of thor

---
+++ src/thor/app.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+class App < Thor
+  namespace :my
+  desc "hoge", "example of hoge"
+  def hoge
+    puts "hello world"
+  end
+
+  desc "fuga", "example of fuga"
+  def fuga(a, b)
+    puts a
+    puts "***"
+    puts b
+  end
+
+  desc "foo", "example metho_option"
+  method_option :file, aliases: "-f", type: :boolean, banner: "HOGEHOGE", desc: "hogehoge-file"
+  def foo
+    puts options[:file]
+  end
+
+end
+
+App.start
