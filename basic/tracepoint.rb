76420dff633b37caa017632a8c2542bdd0fdd754
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2018-04-03 15:08:23 +0900

    Update

--- basic/tracepoint.rb
+++ basic/tracepoint.rb
@@ -2,6 +2,8 @@
 
 TracePoint.trace(:end) do |t|
   puts <<~DEBUG
+  ******
+  type: end
   self: #{t.self}
   lineno: #{t.lineno}
   DEBUG
@@ -9,10 +11,32 @@ end
 
 TracePoint.trace(:b_return) do |t|
   puts <<~DEBUG
+  ******
+  type: b_return
   self: #{t.self}
   lineno: #{t.lineno}
   DEBUG
 end
+
+TracePoint.trace(:line) do |t|
+  puts <<~DEBUG
+  ******
+  type: line
+  self: #{t.self}
+  lineno: #{t.lineno}
+  DEBUG
+end
+
+TracePoint.trace(:raise) do |t|
+  puts <<~DEBUG
+  ******
+  type: raise
+  self: #{t.self}
+  lineno: #{t.lineno}
+  DEBUG
+end
+
+a = :hello
 
 class Hoge
 end
@@ -20,3 +44,4 @@ end
 Class.new do
 end
 
+raise 'hogehoge'
