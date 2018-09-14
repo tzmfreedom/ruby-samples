1a0f29e259b4dc595b458d8204d9dfd94dde2fbd
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2018-09-14 13:45:13 +0900

    update

--- rubocop/custom.rb
+++ rubocop/custom.rb
@@ -2,32 +2,59 @@ module RuboCop
   module Cop
     module Style
       class Custom < Cop
-        MSG = 'foobar'
+        MSG = 'Use variable on rescue clause'
 
-        def on_send(node)
-          receiver, method_name, *args = *node
-          if method_name.to_s == 'hogehoge'
-            add_offense(node)
-          end
-        end
+        # def on_send(node)
+        #   receiver, method_name, *args = *node
+        #   if method_name.to_s == 'hogehoge'
+        #     add_offense(node)
+        #   end
+        # end
+
+        def_node_matcher :res, <<-PATTERN
+          (resbody nil? (lvasgn $...) ...)
+        PATTERN
 
         def on_resbody(node)
           rescued, variable, body = *node
-          if body.send_type?
-            receiver, method_name, *args = *body
-            args.each do |arg|
-              if arg.variable?
-                if arg.children[0] == :e
-                  add_offense(node)
-                end
+          return unless variable
+
+          offense = true
+          checker = VariableChecker.new(variable.children[0])
+
+          if body.send_type? && checker.(body)
+            offense = false
+          elsif body.begin_type?
+            body.children.each do |child|
+              if checker.(child)
+                offense = false
               end
             end
           end
+
+          if offense
+            add_offense(node)
+          end
+        end
+      end
+    end
+
+    class VariableChecker
+      def initialize(variable_name)
+        @variable_name = variable_name
+      end
+
+      def call(node)
+        return true unless node.send_type?
+
+        _, _, *args = *node
+        args.each do |arg|
+          if arg.variable? && arg.children[0] == @variable_name
+            return true
+          end
         end
 
-        def on_lvasgn(node)
-          lhs, rhs = *node
-        end
+        false
       end
     end
   end
--- rubocop/run.rb
+++ rubocop/run.rb
@@ -1,5 +1,45 @@
 begin
-  puts 123 # 3
+  puts 123
+rescue => e
+  puts true
+  puts e
+end
+
+begin
+  puts 123
 rescue => e
   puts e
+end
+
+begin
+  puts 123
+rescue
+  puts true
+end
+
+begin
+  puts 123
+rescue => e
+  puts true
+end
+
+begin
+  puts 123
+rescue => e
+  puts true
+  puts 123
+end
+
+begin
+  puts 123
+rescue SystemCallError, RegexpError => e
+  puts e
+  puts true
+end
+
+begin
+  puts 123
+rescue SystemCallError, RegexpError => e
+  puts true
+  puts false
 end
